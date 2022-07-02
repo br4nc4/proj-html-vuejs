c7a8191cb59f8a363af378a5c29ee31307dd2256
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-07-02 15:35:16 +0200

    icons section menu categories

--- src/components/MenuCategories.vue
+++ src/components/MenuCategories.vue
@@ -15,7 +15,7 @@
             </div>
 
             <!-- IMMAGINI MENU CATEGORIES 4 COLONNE -->
-                <div class="row row-cols-4">
+            <div class="row row-cols-4">
                     <div class="col px-0 d-flex justify-content-center align-items-center">
                         <img class="myImg" src="/img/appetizers-menu-background.jpg" alt="">
                     </div>
@@ -40,7 +40,27 @@
                     <div class="col-3 px-0 d-flex justify-content-center align-items-center">
                         <img class="myImg" src="/img/specials-menu-background.jpg" alt="">
                     </div>
+            </div>
+
+            <!-- 4 ICONE -->
+            <div class="d-flex align-items-center">
+                <div class="icons">
+                    <img src="/img/leaf.png" alt="">
+                    <p>Vegetarian</p>
                 </div>
+                <div class="icons">
+                    <img class="pb-1" src="/img/corn.png" alt="">
+                    <p>Gluten Free</p>
+                </div>
+                <div class="icons">
+                    <img src="/img/milk.png" alt="">
+                    <p>Dairy Free</p>
+                </div>
+                <div class="icons">
+                    <img src="/img/meat.png" alt="">
+                    <p>Keto Friendly</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -83,5 +103,11 @@
     }
     .container{
         max-width: 1032px;
+    }
+    .icons img{
+        transform: scale(0.8)
+    }
+    .icons{
+        text-align: center;
     }
 </style>
